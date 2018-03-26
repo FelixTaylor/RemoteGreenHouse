300becb6d207f340b03f82074976494218c886ec
Felix Taylor <fetalix@gmail.com>
2018-03-26 16:47:48 +0200

    Plant Object
    New Plant object

---
+++ RemoteGreenHouse/app/src/main/java/project/group/remotegreenhouse/GreenHouse/Plant.java
@@ -0,0 +1,39 @@
+package project.group.remotegreenhouse.GreenHouse;
+public class Plant {
+    private static final String TAG = "Plant";
+    private String name;
+    private double moisture;
+
+    public Plant() {
+      setName("NEW PLANT");
+    }
+    public Plant(String name) {
+        this();
+        setName(name);
+    }
+
+    // SETTER
+    public void setName(String name) {
+        if (name != null && !name.isEmpty()) {
+            this.name = name;
+            System.out.println(TAG + ".setName(): Name set to '" + name + "'.");
+        } else {
+            System.err.println(TAG + ".setName(): param is null/empty.");
+        }
+    }
+    public void setMoisture(double moisture) {
+        this.moisture = moisture;
+    }
+
+    // GETTER
+    public String getName() {
+        return this.name;
+    }
+    public double getMoisture() {
+        return this.moisture;
+    }
+
+    public String toString() {
+        return  TAG + " '" + getName() + "'";
+    }
+}
